36338b4d47625bd8ff845dcb9f4ad67bebaf9df0
jenil0909 <110151761+jenil0909@users.noreply.github.com>
2026-07-13 11:42:27 +0530

    Add: majority-element - submission-3

---
+++ Data Structures & Algorithms/majority-element/submission-3.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+     int count = 1;
+     int maxFrequentElement = nums[0];
+     for (int i = 1;i < nums.length; i ++){
+        if (nums[i] == maxFrequentElement){
+            count++;
+        }
+        else {
+            count--;
+            if (count == 0){
+                maxFrequentElement = nums[i];
+                count= 1;
+            }
+        }
+     }
+        return maxFrequentElement;
+    }
+}
